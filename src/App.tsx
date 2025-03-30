5fd93c189fb189c5c82c9c9bf9db776a12a64cd3
ather1234 <shreypal37@gmail.com>
2025-03-30 14:41:21 -0700

    changes:home screen completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { publiclayout as PublicLayout } from '@/layouts/PublicLayout';
-import AuthenticationLayout from '@/layouts/auth-layout';
+import { publiclayout as PublicLayout } from "@/layouts/PublicLayout";
+import AuthenticationLayout from "@/layouts/auth-layout";
 import ProtectRoutes from "@/layouts/protected-routes";
 import { MainLayout } from "@/layouts/main-layout";
 
-import HomePage from '@/routes/home';
-import SignInPage from './routes/sign-in';
-import { SignUpPage } from './routes/sign-up';
+import HomePage from "@/routes/home";
+import SignInPage from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* Public routes */}
-        <Route element = {<PublicLayout />}>
-          <Route index element={<HomePage />} /> 
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
         </Route>
         {/*authhentication layout*/}
-             <Route element = {<AuthenticationLayout />}>
-          <Route path="/signin/*" element={<SignInPage />} /> 
-          <Route path="/signup/*" element={<SignUpPage />} /> 
-
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
         </Route>
 
         {/* Protected routes */}
-        <Route element={<ProtectRoutes><MainLayout/></ProtectRoutes>}>
-         
+        <Route
+          element={
+            <ProtectRoutes>
+              <MainLayout />
+            </ProtectRoutes>
+          }
+        >
           {/*add all the protected routes*/}
-
         </Route>
       </Routes>
     </Router>
--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,23 +1,20 @@
 import { container as Container } from "@/components/container";
-import Footer from "@/components/footer"
-import Header from "@/components/header"
-import { Outlet } from "react-router-dom"
-
-
+import { Footer } from "@/components/footer";
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
   return (
     <div className="w-full">
-          {/*handler to store the user data*/}
-          <Header />
+      {/*handler to store the user data*/}
+      <Header />
 
       <Container className="flex-grow">
         <main className="flex-grow">
-          <Outlet/>
+          <Outlet />
         </main>
       </Container>
-          <Footer />
+      <Footer />
     </div>
-  )
-}
-
+  );
+};
